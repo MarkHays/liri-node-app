67b3854d06e3ed9af56aaa4dfde4ce5379cf0927
MarkHays <mark.hays1991@gmail.com>
2018-12-01 22:25:53 -0800

    saving changes

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # liri-node-app
 How to get stared with the app: If someone wanted to clone your app from github and run it themselves, they would need to supply their own `.env` file for it to work.
+# Introduction
+* I created a node.js app called LIRI. LIRI is like SIRI (from an iphone).
+
+* It must be ran in the command line.
+
+* LIRI is a Language Interpretation and Recognition Interface.
+
+* LIRI will be a command line node app that takes in parameters and gives you back data.
+
+* LIRI will do any of the commands below when you enter them into the command line.
+
+1. spotify-this-song
+1. movie-this
+1. concert-this
+1. do-what-it-says
+
+# Next I will add a screenshot for each command.
+
+*Spotify-This-Song
+
+![spotify-this-song](https://user-images.githubusercontent.com/42901367/49336163-8d185a00-f5b1-11e8-9e89-d8bda2418631.png)
+
+
+*Movie-This
+
+![movie-this](https://user-images.githubusercontent.com/42901367/49336187-234c8000-f5b2-11e8-8bd8-4485f1b850a4.png)
+
+
+*Concert-This
+
+![concert-this](https://user-images.githubusercontent.com/42901367/49336213-d87f3800-f5b2-11e8-9e49-0e90934ce8c7.png)
+
+
+*Do-What-It-Says
+
+
+![do-what-it-says](https://user-images.githubusercontent.com/42901367/49336475-c94eb900-f5b7-11e8-809a-590cba5a007a.png)
--- liri.js
+++ liri.js
@@ -4,99 +4,141 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var x = "";
 var nodeArgv = process.argv;
-var command = process.argv[2];
+var inputCommand = process.argv[2];
+var commandParam = process.argv[3];
 var request = require('request');
 var moment = require('moment');
 var fs = require('fs');
 
-for (var i = 3; i < nodeArgv.length; i++){
-    if (i > 3 && i < nodeArgv.length){
+for (var i = 3; i < nodeArgv.length; i++) {
+    if (i > 3 && i < nodeArgv.length) {
         x = x + "+" + nodeArgv[i];
     } else {
         x = x + nodeArgv[i];
     }
 }
+function processCommands(command, commandParam){
 
 switch (command) {
     case "spotify-this-song":
-   if(x){
-       spotifySong(x);
-   }else {
-       spotifySong("Closer To The Sun");
-   }
-      break;
-    
-    case "movie-this":
-    if(x){
-        omdbMovie(x);
-    }else {
-        omdbMovie("The Mask");
-    }
-      break;
-    
-    case "concert-this":
-    //   withdraw();
-      break;
-    
-    case "do-what-it-says":
-    //   lotto();
-      break;
-    }
-
-function spotifySong(song){
- 
-spotify.search({ type: 'track', query: song, limit:3 }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }else {
-        for (var i = 0; i < data.tracks.items.length; i++){
-            var spotifydata = data.tracks.items[i];
-            console.log("Artist: "+ spotifydata.artists[0].name);
-            console.log("Song: "+ spotifydata.name);
-            console.log("previewURL: "+spotifydata.preview_url);
-            console.log("Album: "+ spotifydata.album.name);
-            console.log("--------------------------------------------------------------------");
-
-            
-
+        if (x) {
+            spotifySong(x);
+        } else {
+            spotifySong("Closer To The Sun");
         }
-       
-    }
-// console.log(spotifydata);
-// console.log(data.tracks);
-});
+        break;
+
+    case "movie-this":
+        if (x) {
+            omdbMovie(x);
+        } else {
+            omdbMovie("The Mask");
+        }
+        break;
+
+    case "concert-this":
+        if (x) {
+            concertThis(x);
+        } else {
+            concertThis("Stick Figure")
+        }
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+        default:
+        break;
+}
 }
 
-function omdbMovie(movieName){
- 
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-    request(queryUrl,function(err, response, body) {
+function spotifySong(song) {
 
-                var body = JSON.parse(body);
-
-                console.log("Title: " + body.Title);
-                console.log("Year: " + body.Year);
-                console.log("Rated: " + body.Rated);
-                console.log("Rotten Tomatoes Rating: " + body.Ratings[1].Value);
-                console.log("Country created: " + body.Country);
-                console.log("Language: " + body.Language);
-                console.log("Plot: " + body.Plot);
-                console.log("Actors: " + body.Actors);
-
-
-                // console.log("Title: "+ spotifydata.artists[0].name);
-                // console.log("Song: "+ spotifydata.name);
-                // console.log("previewURL: "+spotifydata.preview_url);
-                // console.log("Album: "+ spotifydata.album.name);
+    spotify.search({ type: 'track', query: song, limit: 3 }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        } else {
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                var spotifydata = data.tracks.items[i];
+                console.log("Artist: " + JSON.stringify(spotifydata.artists[0].name));
+                console.log("Song: " + spotifydata.name);
+                console.log("previewURL: " + spotifydata.preview_url);
+                console.log("Album: " + spotifydata.album.name);
                 console.log("--------------------------------------------------------------------");
 
-                
-    
 
-           
-  
-    // console.log(spotifydata);
-    // console.log(data.tracks);
+
+            }
+
+        }
+        // console.log(spotifydata);
+        // console.log(data.tracks);
     });
-    }
+}
+
+function omdbMovie(movieName) {
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    request(queryUrl, function (err, response, body) {
+
+        var body = JSON.parse(body);
+
+        console.log("Title: " + body.Title);
+        console.log("Year: " + body.Year);
+        console.log("Rated: " + body.Rated);
+        console.log("Rotten Tomatoes Rating: " + body.Ratings[1].Value);
+        console.log("Country created: " + body.Country);
+        console.log("Language: " + body.Language);
+        console.log("Plot: " + body.Plot);
+        console.log("Actors: " + body.Actors);
+
+
+        // console.log("Title: "+ spotifydata.artists[0].name);
+        // console.log("Song: "+ spotifydata.name);
+        // console.log("previewURL: "+spotifydata.preview_url);
+        // console.log("Album: "+ spotifydata.album.name);
+        console.log("--------------------------------------------------------------------");
+
+
+
+
+
+
+        // console.log(spotifydata);
+        // console.log(data.tracks);
+    });
+}
+
+function concertThis(artist) {
+
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    request(queryUrl, function (err, response, body) {
+
+        var userBand = JSON.parse(body);
+
+        console.log("Name Of Venue: " + userBand[i].venue.name + "\nVenue Location: " + userBand[i].venue.latitude + userBand[i].venue.longitude);
+        let concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");
+        console.log("Date and Time: " + concertDate)
+
+        console.log("--------------------------------------------------------------------");
+    });
+}
+function doWhatItSays(){
+	fs.readFile('random.txt', 'utf8', function(err, data){
+
+		if (err){ 
+			return console.log(err);
+		}
+
+		var dataArr = data.split(',');
+
+		processCommands(dataArr[0], dataArr[1]);
+	});
+}
+
+
+
+//-------------------------MAIN PROCESS-------------------------------------------
+
+processCommands(inputCommand, commandParam);
